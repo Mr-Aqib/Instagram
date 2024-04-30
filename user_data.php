74389e59a8d1674505ddcf01dc04bdb53df6df3f
Aqib1422 <mraqibshafique@gmail.com>
2024-04-30 18:50:03 +0500

    work

--- user_data.php
+++ user_data.php
@@ -3,18 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <?php include './BS-CSS.php'?>
-    <?php include './signup-data.php' ?> -->
+    <?php include './BS-CSS.php'?>
+    <?php include './config.php'?>
     <title>Document</title>
 </head>
 <body>
-    <h1 class="container">Helo</h1>
-    <?php
-    session_start();
-   if(isset($_SESSION['name'])){
-    echo 'Welcome'.$_SESSION['name'].'';
-   }
+   
+        <?php
+        session_start();
+        if(isset($_SESSION['name']))
+        {
+            echo "<h1 class='text-center'> {$_SESSION['name']}</h1>";
+           echo "<a href='./logout-data.php' class='btn btn-success'>Logout</a>";
+        }
+        else{
+            header("Location: $base_url/signup-form.php");
+        }
+        ?>
+
     
-    ?>
 </body>
 </html>
